58a162169ca6ada9251bb0e9d6a95ed6e9b80a93
Eugene <kntece@mail.ru>
2022-06-05 16:39:59 +0300

    Первая домашка C#

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задача 2: Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, 
+а какое меньшее. */
+
+/* Console.WriteLine("Введите число a:");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Int32.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Число a больше числа b");
+    }
+else
+{
+    Console.WriteLine("Число b больше числа a");
+} */
+
+
+
+/* Задача 4: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел. */
+
+/* Console.WriteLine("Введите число a:");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c:");
+int c = Int32.Parse(Console.ReadLine());
+if ((a > b) & (a > c))
+    Console.WriteLine("Число а является наибольшим");
+else if ((b > a) & (b > c))
+    Console.WriteLine("Число b является наибольшим");
+else if ((c > a) & (c > b))
+    Console.WriteLine("Число с является наибольшим"); */
+
+
+
+/* Задача 6: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка). */
+
+/* Console.WriteLine("Введите число:");
+int a = Int32.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число не четное");
+} */
+
+
+
+/* Задача 8: Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N. */
+
+/* Console.WriteLine("Введите число n:");
+int n = Int32.Parse(Console.ReadLine());
+int i = 0;
+while (i <= n)
+    {
+        if (i % 2 == 0)
+        Console.Write(i + " ");
+        i++;
+    } */
+
+
+
+
+
