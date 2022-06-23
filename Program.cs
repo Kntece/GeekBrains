8dca90e0aa610e8c2a0403fcd6aaede7a0716ecb
Kntece <kntece@mail.ru>
2022-06-23 18:29:47 +0300

    Задачи 4

--- Program.cs
+++ Program.cs
@@ -181,4 +181,83 @@ while (i <= n)
     i++;
 } */
 
-///21.06.2022 г.
+
+///___________________________________________________________________________________________________________
+
+
+///Задачи Практического задания #4
+
+/* Задача 25: Напишите цикл, который принимает 
+на вход два числа (A и B) и возводит число A в 
+натуральную степень B. */
+
+/* Console.WriteLine("Задача 25");
+
+int Exponentiation(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation); */
+
+
+///=====================================================
+
+
+/* Задача 27: Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе. */
+
+/* Console.WriteLine("Задача 27");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber); */
+
+
+///=====================================================
+
+
+/* Задача 29: Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран. */
+
+/* int [] array = new int [15];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,100);
+} 
+
+void PrintArray (int [] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write(collection[i] + " ");
+    }
+}
+
+PrintArray(array); */
